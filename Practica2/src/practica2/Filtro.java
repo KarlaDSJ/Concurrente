e82c58456c762492716528e5c58ff0b815c5e4c5
DanyDolphin <danielcastillo59814@ciencias.unam.mx>
2022-04-20 18:05:32 -0500

    practica 2 terminada?

--- Practica2/src/practica2/Filtro.java
+++ Practica2/src/practica2/Filtro.java
@@ -14,16 +14,39 @@ public class Filtro implements Semaforo {
         this.permits = permits;
 
         nivel = new int[totalThreads];
+        victima = new int[totalThreads - permits];
+
+        for (int i = 0; i < totalThreads; i++)
+            nivel[i] = -1;
     }
 
     @Override
-    public void acquire(){}
+    public void acquire(){
+        int ID = Integer.valueOf(Thread.currentThread().getName());
+        for (int l = 0; l < totalThreads - permits; l++) {
+            nivel[ID] = l;
+            victima[l] = ID;
+            while ((victima[l] == ID) && noThreadAboveMe(ID, l));
+        }
+    }
+
+    public boolean noThreadAboveMe(int id, int l) {
+        for (int k = 0; k < totalThreads; k++) {
+            if (k != id && nivel[k] >= l) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     @Override
-    public void release(){}
+    public void release(){
+        int ID = Integer.valueOf(Thread.currentThread().getName());
+        nivel[ID] = -1;
+    }
 
     @Override
     public int getPertitsOnCriticalSections() {
-        return 0;
+        return permits;
     }
 }
