4ff635b19b99e48bd934ec0133a15568f4652808
KarlaDSJ <karla_dsj@ciencias.unam.mx>
2022-06-09 20:45:33 -0500

    Tía Aly 3 hilos y readme

--- Practica3/TiaAly/TiaAly.java
+++ Practica3/TiaAly/TiaAly.java
@@ -8,28 +8,33 @@ public class TiaAly {
         List<Platillo> platillo = new ArrayList<Platillo>();
         platillo.add(new Platillo("Torta",1));
         platillo.add(new Platillo("Tacos Dorados",2));
-        platillo.add(new Platillo("Enchiladas suizas",3));
 
         Queue<Cliente> fila = new LinkedList<>();
         fila.add(new Cliente("Miguel",platillo));
         fila.add(new Cliente("Juan",platillo));
         fila.add(new Cliente("Poncho",platillo));
+        fila.add(new Cliente("Juan",platillo));
+        fila.add(new Cliente("Poncho",platillo));
 
-        Cocinero[] chefs = {new Cocinero("Zeratun"),new Cocinero("Wemi")};
-        Thread thread1, thread2;
+        Cocinero[] chefs = {new Cocinero("Zeratun"),new Cocinero("Wemi"), new Cocinero("Ket")};
+        Thread thread1, thread2, thread3;
 
         while(!fila.isEmpty()){//Mientras tengamos gente en la fila
-            if(fila.size() > 1){//Si hay al menos 2 usamos 2 chefs
+            if(fila.size() > 3){//Si hay al menos 2 usamos 2 chefs
                 chefs[0].setCliente(fila.poll());
                 chefs[1].setCliente(fila.poll());
+                chefs[2].setCliente(fila.poll());
                 thread1 = new Thread(chefs[0]);
                 thread2 = new Thread(chefs[1]);
+                thread3 = new Thread(chefs[2]);
 
                 thread1.start();
                 thread2.start();
+                thread3.start();
 
                 thread1.join();
                 thread2.join();
+                thread3.join();
             }else{//Solo hay 1
                 chefs[0].setCliente(fila.poll());
                 thread1 = new Thread(chefs[0]);
